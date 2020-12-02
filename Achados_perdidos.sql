0339b552fa5409484dd9b84e6beda84864c44e73
Bruno <brunomanarin@icloud.com>
2020-12-02 13:32:21 -0300

    feature: add values into funcionarios

--- Achados_perdidos.sql
+++ Achados_perdidos.sql
@@ -21,16 +21,26 @@ CREATE TABLE Funcionarios
 CREATE TABLE Objetos
 (
     codigo int,
-    descricao    varchar(40) NOT NULL,
-    nome_centro    varchar(40) NOT NULL,
-    categoria    varchar(40) NOT NULL,
+    descricao varchar(40) NOT NULL,
+    nome_centro varchar(40) NOT NULL,
+    categoria varchar(40) NOT NULL,
     PRIMARY KEY (codigo)
-    FOREIGN KEY (categoria) REFERENCES Categorias,
+    FOREIGN KEY (categoria) REFERENCES Categorias
 );
 
 CREATE TABLE Categorias
 (
     codigo int,
     nome    varchar(40) NOT NULL,
-    PRIMARY KEY (CPF)
+    PRIMARY KEY (codigo)
 );
+
+insert into Funcionarios(cpf, nome, funcao)
+VALUES (56108849266, 'Kauan Lima Araujo', 'gerente'),
+(23979180050, 'Maria Almeida Rodrigues', 'auxiliar'),
+(94903499847, 'Lavinia Lima Almeida', 'auxiliar'),
+(60617592152, 'Cauã Lima Silva', 'auxiliar'),
+(90145954765, 'Melissa Rodrigues Goncalves', 'gerente'),
+(65542079024, 'Carlos Cunha Santos', 'auxiliar'),
+(53531125125, 'Luan Correia Goncalves', 'auxiliar'),
+(40263847055, 'Nicolash Almeida Lima', 'gerente');
